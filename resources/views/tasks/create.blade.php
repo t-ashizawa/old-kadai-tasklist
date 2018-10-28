20b5737ac795dfd3181e64bd1ea67e5eee93e3bf
Ashi <ashi@AshinoiMac.local>
2018-10-28 15:13:03 +0900

    styling bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,22 +4,26 @@
 
 <h1>タスク新規作成ページ</h1>
 
+<div class="row">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-sm-offset-2 col-lg-offset-3 col-lg-6 col-offset-3">
+        {!! Form::model($task, ['route' => 'tasks.store']) !!}
+        
+        <div class="form-group">
+            {!! Form::label('status', 'ステータス：') !!}
+            {!! Form::select('status', [
+                'working' => '着手',
+                'completed' => '完了',
+                'pending' => '保留'], null, ['class' => 'form-control']
+                ) !!}
+        </div>
+        <div class="form-group">
+            {!! Form::label('content', 'タスク：') !!}
+            {!! Form::text('content', null, ['class' => 'form-control']) !!}
+        </div>
 
-{!! Form::model($task, ['route' => 'tasks.store']) !!}
-
-{!! Form::label('status', 'ステータス：') !!}
-{!! Form::select('status', [
-    'working' => '着手',
-    'completed' => '完了',
-    'pending' => '保留']
-    ) !!}
-
-{!! Form::label('content', 'タスク：') !!}
-{!! Form::text('content') !!}
-
-{!! Form::submit('作成') !!}
-
-{!! Form::close() !!}
-
+        {!! Form::submit('作成', ['class' => 'btn btn-primary']) !!}
+    </div>  
+        {!! Form::close() !!}
+</div>
 
 @endsection
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,22 +2,30 @@
 
 @section('content')
 
+
 <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
-{!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+<div class="row">
+    <div class="col-xs-6">
+		{!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
+			<div class="form-group">
+				{!! Form::label('status', 'ステータス:') !!}
+				{!! Form::select('status', [
+				    'working' => '着手',
+				    'completed' => '完了',
+				    'pending' => '保留'], null, ['class' => 'form-control']
+				    ) !!}
+			</div>
+			<div class="form-group">
+				{!! Form::label('content', 'タスク：') !!}
+				{!! Form::text('content', null, ['class' => 'form-control']) !!}
+			</div>
+			
+				{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+		
+		{!! Form::close() !!}
+	</div>
+</div>
 
-		{!! Form::label('status', 'ステータス：') !!}
-		{!! Form::select('status', [
-		    'working' => '着手',
-		    'completed' => '完了',
-		    'pending' => '保留']
-		    ) !!}
-
-		{!! Form::label('content', 'タスク：') !!}
-		{!! Form::text('content') !!}
-
-		{!! Form::submit('更新') !!}
-
-{!! Form::close() !!}
 
 @endsection
