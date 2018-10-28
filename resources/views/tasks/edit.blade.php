73b1345f54e10d226a4e9a10c864802cc3a69be6
芦沢哲也 <t.ashizawa.726@gmail.com>
2018-10-28 23:40:59 +0900

    modification css

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,29 +2,28 @@
 
 @section('content')
 
-
 <h1>id: {{ $task->id }} のタスク編集ページ</h1>
 
 <div class="row">
-    <div class="col-xs-6">
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8  col-md-offset-2 col-md-8  col-lg-offset-3 col-lg-6">
 		{!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
-			<div class="form-group">
-				{!! Form::label('status', 'ステータス:') !!}
-				{!! Form::select('status', [
-				    'working' => '着手',
-				    'completed' => '完了',
-				    'pending' => '保留'], null, ['class' => 'form-control']
-				    ) !!}
-			</div>
-			<div class="form-group">
-				{!! Form::label('content', 'タスク：') !!}
-				{!! Form::text('content', null, ['class' => 'form-control']) !!}
-			</div>
-			
-				{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
-				{!! Form::close() !!}
+		
+		<div class="form-group">
+			{!! Form::label('status', 'ステータス:') !!}
+			{!! Form::select('status', [
+			    'working' => '着手',
+			    'completed' => '完了',
+			    'pending' => '保留'], null, ['class' => 'form-control']
+			    ) !!}
+		</div>
+		<div class="form-group">
+			{!! Form::label('content', 'タスク：') !!}
+			{!! Form::text('content', null, ['class' => 'form-control']) !!}
+		</div>
+		
+		{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+		{!! Form::close() !!}
 	</div>
 </div>
-
 
 @endsection
